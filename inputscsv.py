efcae414f243fb4f9c80ed20a90412198046ff3c
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-22 15:10:22 +0100

    Update inputscsv.py

--- inputscsv.py
+++ inputscsv.py
@@ -5,7 +5,7 @@ for i in range(1):
     prenom=input("prenom=?")
     nom=input("nom=?")
     fonction=input("fonction=?")
-    row=[nom,prenom,fonction]
+    row=[prenom,nom,fonction]
     rows.append(row) #ajouter une ligne au tableau
 with open('users.csv', 'w') as f:  #créer et écrire dans le CSV
     write = csv.writer(f) #pointer sur le ficher (créer le fichier)
