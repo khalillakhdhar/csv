17614da669cb9693e43caaf78a150447ea1a456e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-22 15:09:06 +0100

    data

--- inputscsv.py
+++ inputscsv.py
@@ -1,13 +1,13 @@
 import csv 
 header = ['Prenom', 'Nom', 'Fonction']
 rows=[[]]
-for i in range(2):
+for i in range(1):
     prenom=input("prenom=?")
     nom=input("nom=?")
     fonction=input("fonction=?")
     row=[nom,prenom,fonction]
-    rows.append(row)
-with open('users.csv', 'w') as f: 
-    write = csv.writer(f) 
+    rows.append(row) #ajouter une ligne au tableau
+with open('users.csv', 'w') as f:  #créer et écrire dans le CSV
+    write = csv.writer(f) #pointer sur le ficher (créer le fichier)
     write.writerow(header) 
     write.writerows(rows) 
